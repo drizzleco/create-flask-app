d584feb17af5d8487e341365ad0fe57a57b0ac79
Philip Zhang <jmslca123@gmail.com>
2020-08-19 15:48:07 -0700

    setup pypi package

--- setup.py
+++ setup.py
@@ -1,14 +1,31 @@
 import setuptools
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setuptools.setup(
-    name="create-flask-app",
+    name="new-flask-app",
     version="1.0",
+    author="Philip Zhang",
+    author_email="jmslca123@gmail.com",
+    description="Autogenerate boilerplate code for a Flask app",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/drizzleco/create-flask-app",
     packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Intended Audience :: Developers',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    license='MIT',
     install_requires=['PyInquirer', 'jinja2'],
     entry_points={
         'console_scripts': [
-            'create-flask-app=create_flask_app:prompt_user',
+            'create-flask-app=create_flask_app.create_flask_app:prompt_user',
         ],
-    },
-    include_package_data=True,
+    }
     )
