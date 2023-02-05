dcb1a662798e6e55ae6c6e051e705734bc61e232
Philip Zhang <code@philipzha.ng>
2023-02-04 17:12:15 -0800

    bump patch version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="new-flask-app",
-    version="1.0.3",
+    version="1.0.4",
     author="Philip Zhang",
     author_email="jmslca123@gmail.com",
     description="Autogenerate boilerplate code for a Flask app",
